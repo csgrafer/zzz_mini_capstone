169c8c1866afc49e6ed3810ddd3b89f19099459d
“YOUR <2drum4fun@gmail.com>
2019-03-03 08:51:49 -0600

    use segment parameter to show specific product

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/product" => "products#index"
+    get "/product/:product_id" => "products#index"
     get "/all_products" => "products#indices"
   end
 end
