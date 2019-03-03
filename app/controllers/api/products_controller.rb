5db4815cf4036648b27f02e8300806ea41860c5e
“YOUR <2drum4fun@gmail.com>
2019-03-02 23:35:28 -0600

    add second product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @product = Product.first
+    render 'index.json.jbuilder'
+  end
+
+  def indices
+    @products = Product.all
+    render 'indices.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/products" => "products#index"
+    get "/all_products" => "products#indices"
+  end
 end
